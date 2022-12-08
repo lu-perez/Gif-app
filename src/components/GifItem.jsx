b9d9f980852c1071cc56863390a73aaaae7d4b7d
lu-perez <luperez@teco.com.ar>
2022-12-08 18:26:33 -0300

    install prop-types

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const GifItem = ({ title, url }) => {
   return (
     <div className="card">
@@ -8,3 +10,8 @@ const GifItem = ({ title, url }) => {
 }
 
 export default GifItem
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
+}
